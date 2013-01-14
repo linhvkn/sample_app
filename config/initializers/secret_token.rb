70ab5f48bfd2bd52a5dfa38e574c86eccbf2a287
linhvkn <linhvkn.ict@gmail.com>
2013-01-14 15:13:32 +0700

    Initial commit(caixong rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '68ffd8074ec87739bf27ecd426ff4ef006240346742834bc7dba6fa323709425ec920aa7935a6c75153cd7bb29f8bcc15070f10df5bf0452b75b2635585be1db'
